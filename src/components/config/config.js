102b21027e8b3d23a4dbf36b2f4293b9fac0b7ff
Jhune Carlo Trogelio <jhunecarlo.trogelio>
2021-12-05 12:35:07 +0800

    added default config for the API_URL

---
+++ src/components/config/config.js
@@ -0,0 +1,10 @@
+const API_URLS = [
+    "https://anyx.io",
+    "https://api.hive.blog",
+    "https://api.deathwing.me",
+    "https://api.openhive.network"
+]
+
+const HIVE_ENGINE_API_HOST = "https://api.hive-engine.com/rpc/contracts"
+
+export { API_URLS, HIVE_ENGINE_API_HOST }
