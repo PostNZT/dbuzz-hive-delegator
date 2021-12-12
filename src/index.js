fe264945f8832d923df61e105a0682289cb3fa86
Jhune Carlo Trogelio <jhunecarlo.trogelio>
2021-12-13 00:28:35 +0800

    initialized monitoring account users

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+async function monitorAccountUsers() {
+    console.log(`#1: Monitor new and inactive referred account users`)
+}
+
+
 async function main() {
     // add the main method for the application.
 }
