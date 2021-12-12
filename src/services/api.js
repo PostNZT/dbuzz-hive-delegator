bd1c6a39e9ecfb63d6486b7b3f4bdd8a2d06875a
Jhune Carlo Trogelio <jhunecarlo.trogelio>
2021-12-13 00:29:06 +0800

    added getOperationWorker and streamOperations methods

---
+++ src/services/api.js
@@ -0,0 +1,43 @@
+import hiveInterface from '../config/hiveInterface'
+
+export async function streamOperations(callbacks, options = {}) {
+    return await hiveInterface.stream({
+        on_op: (op, block_num, block_id, previous_block, trx_id, timestamp) => {
+            const operation = {
+                trx_id,
+                timestamp,
+                op,
+            }
+
+            if (callbacks && callbacks.length > 0) {
+                callbacks.forEach(callback => {
+                    callback(operation)
+                })
+            }
+        }
+    })
+}
+
+export function getOperationWorker(op) {
+    const name = op.op[0]
+    const params = op.op[1]
+
+    switch(name) {
+        case 'vote':
+            return params.voter
+        case 'comment':
+            return params.author
+        case 'transfer':
+            return params.from
+        case 'custom_json':
+            if (params.required_auths && params.required_auths.length > 0) {
+                return params.required_auths[0]
+            } else if (params.required_posting_auths && params.required_posting_auths.length > 0) {
+                return params.required_posting_auths[0]
+            } else {
+                return null
+            }
+        default: 
+            return null
+    }
+}
