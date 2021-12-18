968943d3bd85904e32dd54378a00f3c3f14dc1d7
Jhune Carlo Trogelio <jhunecarlo.trogelio>
2021-12-19 05:38:57 +0800

    added the stream options

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import { streamOperations } from './services/api'
+ 
 async function monitorAccountUsers() {
     console.log(`#1: Monitor new and inactive referred account users`)
+    console.log(`Monitoring stream operation started!`)
+    await streamOperations([whenReferredUserCreated, whenReferredUserTakeActionss])
+
 }
 
 
 async function main() {
-    // add the main method for the application.
+    await Promise.all([
+        monitorAccountUsers(),
+    ])
 }
 
 main()
