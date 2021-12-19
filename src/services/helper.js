4e0ec2716fb2f1922155b8de00be0ae1e68f360e
Jhune Carlo Trogelio <jhunecarlo.trogelio>
2021-12-19 22:40:30 +0800

    added methods for helper functions for account creation

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,3 +1,5 @@
+import { addToReferredUsers } from '../services/api'
+
 export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 export const parseNumber = (data) => {
@@ -6,4 +8,20 @@ export const parseNumber = (data) => {
     }
     
     return parseFloat(data)
+}
+
+let referredUsers = {}
+
+export function getUser(username) {
+    return referredUsers[username]
+}
+
+export function updateUser(user) {
+    referredUsers = addToReferredUsers([user])
+    console.log(`Updated user data for ${user.account}`)
+}
+
+export function containsUser(username) {
+    const usernames = Object.keys(referredUsers)
+    return usernames.includes(username)
 }
