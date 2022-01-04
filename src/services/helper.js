a7e8f08cbfb8652c2c01598075ae72f9a0281536
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2022-01-04 15:11:58 +0800

    added multiple methods to work on delegating HP on user

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,3 +1,7 @@
+import { STATUS } from '../services/constants'
+import hiveClient from '../config/hive'
+import dHiveClient from '../config/dHive'
+import config from '../../config.json'
 import { addToReferredUsers } from '../services/api'
 
 export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
@@ -24,4 +28,65 @@ export function updateUser(user) {
 export function containsUser(username) {
     const usernames = Object.keys(referredUsers)
     return usernames.includes(username)
+}
+
+export function hasBeneficiarySetting(account, referrer) {
+    let jsonMetadata = account.json_metadata
+    if (jsonMetadata) {
+        jsonMetadata = JSON.parse(jsonMetadata)
+        const beneficiaries = jsonMetadata.beneficiaries
+        if (beneficiaries && beneficiaries.length > 0) {
+            const referred = beneficiaries.filter(b => b.name === referrer)
+            if (referred && referred.length > 0 && referred[0].label === 'referrer') {
+                return referred[0]
+            }
+        }
+    }
+
+    return false
+}
+
+export async function getAccount(account) {
+    return new Promise((resolve, reject) => {
+        hiveClient.api.getAccounts([account], function (err, res) {
+            if (err) {
+                reject(err)
+            } else {
+                if (res && res.length > 0) {
+                    resolve(res[0])
+                } else {
+                    resolve({})
+                }
+            }
+        })
+    })
+}
+
+export async function ownedPower(username) {
+    const account = await getAccount(username) 
+    const avail = parseFloat(account.vesting_shares)
+
+    const props =  await dHiveClient.database.getDynamicGlobalProperties()
+    const vestHive = parseFloat(parseFloat(props.total_vesting_fund_hive) * (parseFloat(avail) / parseFloat(props.total_vesting_shares)), 6)
+    return vestHive
+}
+
+async function getDelegatedUsers() {
+    const users = Object.values(referredUsers)
+    return users.filter(user => user.status === STATUS.DELEGATED).map(user => user.account)
+}
+
+export async function hasDelegatedTo(username) {
+    const delegated = await getDelegatedUsers()
+    if (delegated.includes(username)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+export async function hasEnoughHP(username) {
+    const hp = await ownedPower(username)
+    const maxHP = parseFloat(config.maximumUserHivePower) || 30
+    return hp >= maxHP
 }
