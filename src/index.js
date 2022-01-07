f8cfa937eb272c93f632b35f291bf75217943c2a
Jhune Carlo Trogelio <jhunecarlo.trogelio@gmail.com>
2022-01-07 13:57:00 +0800

    added getdelegatedusers method

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import { streamOperations } from './services/api'
+import { getDelegatedUsers } from './services/helper'
 import { whenReferredUserCreated, whenReferredUserTakeActions } from './components/streamingOperations'
  
 async function monitorAccountUsers() {
     console.log(`#1: Monitor new and inactive referred account users`)
     console.log(`Monitoring stream operation started!`)
     await streamOperations([whenReferredUserCreated, whenReferredUserTakeActions])
+}
 
+async function monitorDelegatedUsers() {
+    console.log(`#2 Monitor delegated users status`)
+    const job = async () => {
+        const users = await getDelegatedUsers()
+        console.log(`DBuzz have delegated to ${users.length} users`, users)
+        if (users.length > 0) {
+            await Promise.all(users.map(user => removeDelegationIfNeeded(user)))
+            console.log(`Checked delegated users status: DONE!`)
+        }
+    }
 }
 
 
 async function main() {
     await Promise.all([
         monitorAccountUsers(),
+        monitorDelegatedUsers(),
     ])
 }
 
